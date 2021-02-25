3a44b67088bc1cb817a977e859d209777f25a62c
Natalia <tashamouse@yandex.ru>
2021-02-25 11:44:01 +0300

    L1 HW is done

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+//Задание 1
+let c = +prompt("Введите температуру в градусах по Цельсию");
+alert("Tемпература в градусах по Фаренгейту: " + ((9 / 5) * c + 32).toFixed(1));
+
+//Задание 2
+let name;
+let admin;
+name = "Василий";
+admin = name;
+alert(admin);
+
+//Задание 3
+let a = 1000;
+let b = "108";
+alert(a + b);
